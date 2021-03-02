e3f0a3821bf2daf264a6ca7aeac259ee6a9a073a
NZ26-06 <71227725+NZ26-06@users.noreply.github.com>
2021-03-02 19:25:32 +0100

    Add files via upload

---
+++ qcd.php
@@ -0,0 +1,27 @@
+<?php
+
+$jsonData='[
+  {"Q":"Le Javascript est le même language que Java","R":"faux","J":"C est un langage a part, cote client"},
+  {"Q":"Jason permet de passer des infos entre serveur et client","R":"faux","J":"C est JSON, pas JASON"},
+  {"Q":"POO signifie Programmation Oriente Objet ","R":"vrai","J":"oui c est vrai"},
+  {"Q":"La DOM permet de changer le contenu d un tableau","R":"faux","J":"Il faut utiliser la TOM"},
+  {"Q":"Peut on utiliser un fichier externe JSON","R":"vrai","J":"Oui par exemple avec AJAX"},
+  {"Q":"JS permet de changer le style d element HTML","R":"vrai","J":"On peut utiliser getElementById"},
+  {"Q":"DJANGO est un framework JS","R":"faux","J":"C est du python"},
+  {"Q":"Javascript est il un techno cote client","R":"vrai","J":"Oui c est bien vrai"},
+  {"Q":"JS permet d afficher la date d aujourd hui","R":"vrai","J":"Oui c est bien vrai"},
+  {"Q":"JS permet de traiter les dates en fonction du format local","R":"R","J":"Oui,le formatage peut etre defini dans le programme"},
+  {"Q":"Brendan Heich est il l inventeur de JS","R":"faux","J":"C est Brendan Eich"},
+  {"Q":"L autre nom est il ECMA-262","R":"vrai","J":"Oui c est le nom de la norme"},
+  {"Q":"Faut il definir les types en JS","R":"faux","J":"Non pas besoin, mais le langage supporte des types tels que Boolean, etc"},
+  {"Q":"le mot let donne une portee gloable","R":"faux","J":"non c est une portee de bloc"},
+  {"Q":"JQUERY est il un framework JS","R":"vrai","J":"Oui c est bien le cas"},
+  {"Q":"<scripting> est il la balise d insertion de code JS","R":"faux","J":"C est <script>"},
+  {"Q":"Peut on declencher des fonctions JS sur des evenements","R":"vrai","J":"Oui, exemple onclick"},
+  {"Q":"L operateur == permet d assigner une valeur ","R":"faux","J":"Il faut utiliser = et pas =="},
+  {"Q":"React.js est il un framework JS","R":"vrai","J":"Oui c est la version conçu par Facebook"},
+  {"Q":"Le code JS doit etre compile","R":"faux","J":"Non ce n est pas un langage "}
+]';
+
+echo $jsonData;
+?>
